e7477451f2b4cd560cf1907de2040b5712b2fc49
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-03-15 20:45:48 +0300

    week 3 first push

---
+++ src/test/java/officeHour/week3/PropertiesReview.java
@@ -0,0 +1,28 @@
+package officeHour.week3;
+
+import org.testng.annotations.*;
+
+import java.io.*;
+import java.util.*;
+
+public class PropertiesReview {
+
+
+    @Test
+    public void test1() throws IOException {
+        System.out.println(System.getProperty("os.name"));
+//        System.out.println(System.getProperty("java.version"));
+//        System.out.println(System.getProperty("java.compiler"));
+
+        Properties properties = new Properties();
+        FileInputStream fis = new FileInputStream("Configuration.properties");
+        properties.load(fis);
+
+        Object object = new Object();
+
+        System.out.println(properties.getProperty("java.version"));
+        System.out.println(properties.getProperty("beymen_url"));
+
+
+    }
+}
---
+++ src/test/java/officeHour/week3/TestNgReview.java
@@ -0,0 +1,37 @@
+package officeHour.week3;
+
+import org.testng.annotations.*;
+
+public class TestNgReview {
+
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("before class");
+    }
+
+    @AfterClass
+    public void afterClass(){
+        System.out.println("after class");
+    }
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("before method");
+
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("after method");
+
+    }
+    @Test
+    public void method1(){
+        System.out.println("Test 1");
+    }
+
+    @Test
+    public void method2(){
+        System.out.println("Test 2");
+
+    }
+}
