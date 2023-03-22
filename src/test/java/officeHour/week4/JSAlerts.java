b4ced091f30ac280e59109e11840889639ff2960
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-03-22 20:45:46 +0300

    week 4 second push

--- src/test/java/officeHour/week4/JSAlerts.java
+++ src/test/java/officeHour/week4/JSAlerts.java
@@ -1,4 +1,46 @@
 package officeHour.week4;
 
-public class JSAlerts {
+import org.openqa.selenium.*;
+import org.testng.*;
+import org.testng.annotations.*;
+
+public class JSAlerts extends TestBase {
+
+
+    // 1 . alert
+    @Test
+    public void test1() throws InterruptedException {
+        driver.get("https://practice.cydeo.com/javascript_alerts");
+        driver.findElement(By.xpath("//button[.='Click for JS Alert']")).click();
+        Alert alert = driver.switchTo().alert();
+        System.out.println(alert.getText());
+        alert.accept();
+        Thread.sleep(2000);
+    }
+
+    // 2. confirm alert
+    @Test
+    public void test2() throws InterruptedException {
+        driver.get("https://practice.cydeo.com/javascript_alerts");
+        driver.findElement(By.xpath("//button[.='Click for JS Confirm']")).click();
+        Alert alert = driver.switchTo().alert();
+        alert.dismiss();
+        Thread.sleep(2000);
+    }
+
+    //3. prompt alert
+    @Test
+    public void test3() throws InterruptedException {
+        String input = "EU10 office hour";
+        driver.get("https://practice.cydeo.com/javascript_alerts");
+        driver.findElement(By.xpath("//button[.='Click for JS Prompt']")).click();
+        Alert alert = driver.switchTo().alert();
+        alert.sendKeys(input);
+        alert.accept();
+
+        String actual = driver.findElement(By.id("result")).getText();
+        Assert.assertTrue(actual.contains(input));
+        Thread.sleep(2000);
+    }
+
 }
